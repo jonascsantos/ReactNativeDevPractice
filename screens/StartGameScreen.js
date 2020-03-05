f7d1fbe419707603a9f1fbb09dab89219a32e475
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-05 22:41:08 +0100

    Input style

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native'
+import { View, StyleSheet, Text, Button } from 'react-native'
 
 import Card from '../components/Card'
+import Input from '../components/Input'
 import Colors from '../constants/colors'
 
 
@@ -11,7 +12,7 @@ const StartGameScreen = props => {
             <Text style={styles.title}> Start a New Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input style={styles.input}/>
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
                         <Button title="Reset" onPress={() => { }} color={Colors.accent} />
@@ -52,7 +53,13 @@ const styles = StyleSheet.create({
 
     button: {
         width: 100,
-    }
+    },
+
+    input: {
+        width: 50,
+        textAlign: 'center',
+        
+    },
 
 
 
