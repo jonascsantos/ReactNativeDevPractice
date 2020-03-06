96af62ab20aacffd989d3629fdf00eb969b31610
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-06 17:42:00 +0100

    Card Start Game

---
+++ screens/GameScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const GameScreen = props => {
+
+};
+
+const style = StyleSheet.create({
+
+
+});
+
+export default GameScreen;
